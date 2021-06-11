f8426bf118baffbd29e6ad26e5a5c00d8a6a1722
xuantruong2472001 <84957708+xuantruong2472001@users.noreply.github.com>
2021-06-11 11:52:47 +0700

    Create module.js

---
+++ module.js
@@ -0,0 +1,25 @@
+
+
+const calc = (a,b,p) => {
+    if(!a || !b || !p){
+        return "Please provide all parameters";
+    }
+    else{
+        switch(p){
+            case '+':
+                return a + b;
+                break;
+            case '-':
+                return a - b;
+                break;
+            case '*':
+                return a * b;
+                break;
+            case '/':
+                return a / b;
+                break;
+            default: {return;}
+        }
+    }
+}
+exports.calc = calc;
